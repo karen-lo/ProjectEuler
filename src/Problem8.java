c2f4cfe0890ed6c28a89589a8e5101cbd1f33e9e
Karen Lo <k8lo@ucsd.edu>
2015-11-24 10:17:21 -0800

    Fixed problem 8

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,28 +1,18 @@
-/* In progress...
- * Project Euler Problem 8: Largest Product In a Series
+/* Project Euler Problem 8: Largest Product In a Series
  * Finds the largest product of n consecutive numbers in a given series .
  */
 import java.util.*;
 import java.io.*;
 
 public class Problem8 {
-	public static void main(String[] args) {
-		int numOfConseqFac = 4;
-		int answer;
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println(System.getProperty("user.dir"));
+		int numOfConseqFac = 13;
+		long answer;
 		String file = new String("Problem8num.txt");
-
-		answer = greatestProduct(file, numOfConseqFac);
-		System.out.println(answer);
-	}
-	
-	public static int greatestProduct(String file, int numOfFactors) throws FileNotFoundException {
-		int product = 1;
-		int maxProd = 1;
-		int iterator = 0;
-		int count = 0;
-		ArrayList<Integer> arraylist = new ArrayList<Integer>();
 		Scanner scanner;
 		
+		//try to make scanner
 		try {
 			scanner = new Scanner(new File(file));
 		} catch (FileNotFoundException e) {
@@ -30,29 +20,59 @@ public class Problem8 {
 			throw e;
 		}
 		
-		while(scanner.hasNextInt()) {
-			arraylist.add(scanner.nextInt());
+		String num = scanner.next();
+		
+		answer = greatestProduct(numOfConseqFac, num);
+		
+		System.out.printf("The answer is %d.", answer);
+		scanner.close();
+	}
+	
+	public static long greatestProduct(int numOfFactors, String num) {
+		long product = 1;
+		long maxProd = 1;
+		int iterator = 0;
+		int count = 0;
+		ArrayList<Integer> arraylist = new ArrayList<Integer>();	
+		
+		//place each individual number into the arraylist
+		for(int x=0; x<num.length(); x++) {
+			
+			int temp = Integer.parseInt(num.substring(x, x+1));
+			arraylist.add(temp);
 		}
+		
 
 		while(iterator < arraylist.size()) {
-			while(count<numOfFactors) {
+			
+			while(count < numOfFactors && iterator < arraylist.size()) {
+				
 				if(arraylist.get(iterator) == 0) {
 					iterator++; 
 					count = 0;
 					product = 1;
 				} else {
-					product = product*arraylist.get(iterator);
+					product *= arraylist.get(iterator);
 					count++;
 					iterator++;
 				}
 			}
+			System.out.printf("The new product is %d.\n", product);
 			
 			if(product > maxProd) {
 				maxProd = product;
+				System.out.printf("The max product is %d.\n", maxProd);
 			}
 			
 			product = 1;
-			iterator-=(numOfFactors-(numOfFactors-2));
+			count = 0;
+			
+			//reset back to the second integer for the next set of factors
+			if(iterator < arraylist.size()-numOfFactors){
+				iterator-=(numOfFactors-1);
+			} else {
+				break;
+			}
 		}
 		
 		return maxProd;
