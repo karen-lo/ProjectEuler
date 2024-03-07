36e03741806aebd6dd0d4cf87a90efb8457eb485
John H. Freeman <john@freemania.pro>
2024-03-07 00:28:48 -0500

    Update Problem12.java

--- src/Problem12.java
+++ src/Problem12.java
@@ -5,7 +5,7 @@
 
 public class Problem12 {
     public static void main(String[] args) {
-        int minDivisors = 500;
+        int minDivisors = Integer.parseInt(args[0]);
         System.out.println(triangleNumFinder(minDivisors));
     }
 
